61bf94bcc65a80a56fde5a9cfd1d90b431570710
marcosviniciusjau <marcosviniciusjahu@gmail.com>
2023-04-11 15:11:42 -0300

    aprendizado rocketseat

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,56 @@
+import React,{ useState ,useEffect} from 'react';
+import './styles.css'
+
+import { Card } from '../../components/Card'
+
+export function Home(name) {
+
+  const [studentName, setStudentName]= useState('');
+  const [students,setStudents] = useState([]);
+
+  function handleAddStudent(){
+     const newStudent={
+      name: studentName,
+      time: new Date().toLocaleDateString("pt-br",{
+        hour:'2-digit',
+        minute:'2-digit',
+        second:'2-digit',
+      })
+     };
+
+     setStudents(prevState=> [...prevState,newStudent]);
+  }
+
+  useEffect(()=>{
+
+  },[]);
+  
+  return (
+    <div className="container">
+   <header>
+   <h1>Lista de Presença</h1>
+   <div>
+    <strong> Rodrigo</strong>
+    <img src="https://github.com/rodrigorgtic.png" alt="Foto de Perfil"/>
+   </div>
+   </header> <input 
+      type="text" 
+      placeholder="Digite o nome..."
+      onChange={e => setStudentName(e.target.value)}/>
+    <button type="button"
+    onClick={handleAddStudent}>Adicionar</button>
+
+    {
+      students.map(student => 
+      (
+      <Card 
+        key={student.time}
+        name={student.name} 
+        time={student.time}
+      />
+      )
+     )}   
+      </div>
+    
+  )
+}
